ffd3ea0bd8a7f092e1742c48568c2350ffefdf47
Your Name <your@email.com>
2026-02-23 10:41:06 +0000

    UPDATE: ZSH key.

--- home/wsl.nix
+++ home/wsl.nix
@@ -5,7 +5,7 @@
     DOCKER_APPS_DATA_PATH = "/mnt/d/apps/docker_apps";
   };
 
-  programs.zsh.initExtra = ''
+  programs.zsh.initContent = ''
     alias pbpaste="powershell.exe -Command \"Get-Clipboard\""
     alias pbcopy="powershell.exe -NoProfile -Command \"[Console]::In.ReadToEnd() | Set-Clipboard\""
   '';
